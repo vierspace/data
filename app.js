a4144f87118959de7b685fa30f079c1f46466593
vierspace <65929916+vierspace@users.noreply.github.com>
2025-04-22 12:55:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("data/serviceWorker.js")
+      .register("https://vierspace.github.io/data/serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
